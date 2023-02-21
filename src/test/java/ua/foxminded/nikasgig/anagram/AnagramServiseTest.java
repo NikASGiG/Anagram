1f5a6b5128c33968f24fa7f7d7fa102117b36ffd
NikASGiG <70024950+NikASGiG@users.noreply.github.com>
2023-02-21 14:25:58 +0200

    fix code style and format

--- src/test/java/ua/foxminded/nikasgig/anagram/AnagramServiseTest.java
+++ src/test/java/ua/foxminded/nikasgig/anagram/AnagramServiseTest.java
@@ -1,35 +1,44 @@
 package ua.foxminded.nikasgig.anagram;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class AnagramServiseTest {
-
     private AnagramService anagramService = new AnagramService();
-    private final String SPACE = " ";
-    private final String FIVE_SPACES = "     ";
-
-    @ParameterizedTest
-    @NullSource
-    void isBlank_ShouldReturnTrueForNullInputs(String input) {
-        assertEquals(input, anagramService.convertLine(input));
+    
+    @Test
+    public void convertLine_whenGivenNull_ReturnNull() {
+        String actual = anagramService.convertLine(null);
+        assertEquals(null, actual);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "", SPACE, FIVE_SPACES })
-    void convertLine_whenGivenString_ReturnsTheSameString(String input) {
-        assertEquals(input, anagramService.convertLine(input));
+    @ValueSource(strings = { 
+            "", 
+            " ",
+            "     ",
+            "a,a",
+            "aaaaaaaaaa,aaaaaaaaaa",
+            "!!!111222333,!!!111222333"})
+    public void convertLine_whenGivenString_ReturnsTheSameString(String input) {
+        String actual = anagramService.convertLine(input);
+        assertEquals(input, actual);
     }
 
     @ParameterizedTest
-    @CsvSource({ "a,a", "aaaaaaaaaa,aaaaaaaaaa", "ttYtrE123wQ OPIp,QwErtY123tt pIPO", "opip," + FIVE_SPACES + "pipo",
-            "!!!111222333,!!!111222333", "ttre123wq opip,qwer123tt pipo" + FIVE_SPACES,
-            "ttre123wq aaao!1122344pip,qwer123tt pipo!1122344aaa", })
-    void convertLine_whenGivenString_ReturnsSuccessAnagrmedString(String expected, String input) {
-        assertEquals(expected, anagramService.convertLine(input));
+    @CsvSource({  
+        "hello,olleh",
+        "tree trees leaf leafes big bigger,eert seert fael sefael gib reggib",
+        "QwErtY123tt pIPO,ttYtrE123wQ OPIp", 
+        "     pipo,opip", 
+        "qwer123tt pipo     ,ttre123wq opip",
+        "qwer123tt pipo!1122344aaa,ttre123wq aaao!1122344pip,", })
+    public void convertLine_whenGivenString_ReturnsSuccessAnagrmedString(String input, String expected) {
+        String actual = anagramService.convertLine(input);
+        assertEquals(expected, actual);
     }
-
 }
